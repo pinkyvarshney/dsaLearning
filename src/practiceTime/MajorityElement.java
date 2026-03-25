2804e399fe6ba5796669ffc427255506aa8928cd
pinly <pinkyvarshney32@gmail.com>
2026-03-25 21:31:52 +0530

    firkuchhst commit

--- src/practiceTime/MajorityElement.java
+++ src/practiceTime/MajorityElement.java
@@ -2,24 +2,30 @@ package practiceTime;
 
 public class MajorityElement {
     public static void main(String[] args) {
-        int a[]={1,2,1,3,1,4,1,1};
-        int val;
-        int count=0;
-        for(int i=0;i<a.length-1;i++){
-         if(count==0){
-             val=a[i];
-         }
-         else if(a[i]==val){
-              count++;
-          }
-          else{
-             count--;
-          }
+        int a[] = {1, 2, 1, 3, 1, 4, 1, 1};
+        int count = 0;
+        int val=0;
+        for (int i = 0; i < a.length; i++){
+            if (count == 0) {
+                val = a[i];
+                count=1;
+            } else if (a[i] == val) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+            count = 0;
+            for (int i = 0; i < a.length; i++){
+              if (a[i] == val) {
+            count++;
+        }
+    }
+            if (count > a.length / 2) {
+                    System.out.println(val);
+                }
+            System.out.println("-1");
+            }
+
         }
 
-        if(count>a.length-1/2){
-            System.out.println(a[i]);
-        }
-        System.out.println("-1");
-    }
-}
