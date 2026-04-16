ea1bfbc5d6cfc4661dcad26ed435282eb2190a58
pinly <pinkyvarshney32@gmail.com>
2026-04-16 22:22:19 +0530

    first commit

---
+++ src/practiceTime/SubArraySumEqualK.java
@@ -0,0 +1,28 @@
+package practiceTime;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubArraySumEqualK {
+    public static void main(String[] args) {
+        int nums []={1,1,1};
+        int k=2;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int sum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        System.out.println(count);
+    }
+}
