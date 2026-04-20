393ff2f5683bc05a3a3a465673264ce360d1de9d
pinly <pinkyvarshney32@gmail.com>
2026-04-20 19:33:53 +0530

    first commit

---
+++ src/practiceTime/TwoSum.java
@@ -0,0 +1,15 @@
+package practiceTime;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+
+        int a[] = {3, 4, 5, 6, 7};
+        int low = 0;
+        int target=9;
+        int high = a.length - 1;
+        while (low < high) {
+
+        }
+    }
+}
